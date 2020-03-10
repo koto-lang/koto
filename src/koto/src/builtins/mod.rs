f03e4f12e845bfc5ea84aa82ec62a2b45021bc67
irh <ian.r.hobson@gmail.com>
2020-03-10 22:19:03 +0100

    Add math.sum

--- src/koto/src/builtins/mod.rs
+++ src/koto/src/builtins/mod.rs
@@ -3,6 +3,7 @@ use koto_parser::vec4;
 use std::{fs, path::Path, rc::Rc};
 
 mod list;
+mod math;
 
 #[macro_export]
 macro_rules! single_arg_fn {
@@ -39,45 +40,7 @@ pub fn register<'a>(runtime: &mut Runtime<'a>) {
     let global = runtime.global_mut();
 
     list::register(global);
-
-    {
-        let mut math = ValueMap::new();
-
-        macro_rules! math_fn_1 {
-            ($fn:ident) => {
-                math_fn_1!(stringify!($fn), $fn)
-            };
-            ($name:expr, $fn:ident) => {
-                single_arg_fn!(math, $name, Number, n, { Ok(Number(n.$fn())) });
-            };
-        }
-
-        math_fn_1!(abs);
-        math_fn_1!(acos);
-        math_fn_1!(asin);
-        math_fn_1!(atan);
-        math_fn_1!(ceil);
-        math_fn_1!(cos);
-        math_fn_1!(cosh);
-        math_fn_1!("degrees", to_degrees);
-        math_fn_1!(exp);
-        math_fn_1!(exp2);
-        math_fn_1!(floor);
-        math_fn_1!(log10);
-        math_fn_1!(log2);
-        math_fn_1!(ln);
-        math_fn_1!("radians", to_radians);
-        math_fn_1!(recip);
-        math_fn_1!(sin);
-        math_fn_1!(sinh);
-        math_fn_1!(sqrt);
-        math_fn_1!(tan);
-        math_fn_1!(tanh);
-
-        math.add_value("pi", Number(std::f64::consts::PI));
-
-        global.add_map("math", math);
-    }
+    math::register(global);
 
     {
         let mut map = ValueMap::new();
