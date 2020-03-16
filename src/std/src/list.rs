c324368f0538bf9f0c6d005c86e8ff91c9aad47b
irh <ian.r.hobson@gmail.com>
2020-03-16 12:46:43 +0100

    Extract common code for list ops, and replace list.add with list.push

--- src/std/src/list.rs
+++ src/std/src/list.rs
@@ -1,38 +1,11 @@
-use crate::{builtin_error, single_arg_fn,};
-use koto_runtime::{
-     value, value::deref_value, Error, Value, ValueList, ValueMap,
-};
+use crate::{builtin_error, single_arg_fn};
+use koto_runtime::{value, value::deref_value, Error, RuntimeResult, Value, ValueList, ValueMap};
 use std::rc::Rc;
 
 pub fn register(global: &mut ValueMap) {
     use Value::*;
 
     let mut list = ValueMap::new();
-
-    list.add_fn("add", |_, args: &[Value]| {
-        let mut arg_iter = args.iter();
-        let first_arg_value = match arg_iter.next() {
-            Some(arg) => arg,
-            None => {
-                return builtin_error!("Missing list as first argument for list.add");
-            }
-        };
-
-        match first_arg_value {
-            List(list) => {
-                let mut list = list.clone();
-                let list_data = Rc::make_mut(&mut list).data_mut();
-                for value in arg_iter {
-                    list_data.push(value.clone())
-                }
-                Ok(List(list))
-            }
-            unexpected => builtin_error!(
-                "list.add is only supported for lists, found {}",
-                value::type_as_string(unexpected)
-            ),
-        }
-    });
 
     single_arg_fn!(list, "is_sortable", List, l, {
         Ok(Bool(list_is_sortable(&l)))
@@ -48,189 +21,148 @@ pub fn register(global: &mut ValueMap) {
         }
     });
 
-    list.add_fn("fill", |_, args| {
-        if args.len() != 2 {
-            return builtin_error!("list.fill expects two arguments, found {}", args.len());
-        }
+    list.add_fn("push", |_, args: &[Value]| {
+        list_op(args, 2, "push", |list| {
+            list.data_mut().extend(args[1..].iter().cloned());
+            Ok(Value::Empty)
+        })
+    });
 
-        match &args[0] {
-            Ref(r) => {
-                match &mut *r.borrow_mut() {
-                    List(l) => {
-                        let value = args[1].clone();
-                        for v in Rc::make_mut(l).data_mut().iter_mut() {
-                            *v = value.clone();
-                        }
-                    }
-                    unexpected => {
-                        return builtin_error!(
-                            "list.fill expects a reference to a\
-                                 list as its first argument, found {}",
-                            value::type_as_string(&unexpected)
-                        )
-                    }
-                }
-                Ok(Value::Empty)
+    list.add_fn("fill", |_, args| {
+        list_op(args, 2, "fill", |list| {
+            let value = args[1].clone();
+            for v in list.data_mut().iter_mut() {
+                *v = value.clone();
             }
-            unexpected => builtin_error!(
-                "list.fill expects a reference to a list as its first argument, found {}",
-                value::type_as_string(unexpected)
-            ),
-        }
+            Ok(Value::Empty)
+        })
     });
 
     list.add_fn("filter", |runtime, args| {
-        if args.len() != 2 {
-            return builtin_error!("list.filter expects two arguments, found {}", args.len());
-        }
-
-        match &args[0] {
-            Ref(r) => {
-                match &mut *r.borrow_mut() {
-                    List(l) => match &args[1] {
-                        Function(f) => {
-                            if f.args.len() != 1 {
-                                return builtin_error!(
-                                    "The function passed to list.filter must have a \
+        list_op(args, 2, "filter", |list| {
+            match &args[1] {
+                Function(f) => {
+                    if f.args.len() != 1 {
+                        return builtin_error!(
+                            "The function passed to list.filter must have a \
                                          single argument, found '{}'",
-                                    f.args.len()
-                                );
-                            }
-                            let mut write_index = 0;
-                            for read_index in 0..l.data().len() {
-                                let value = l.data()[read_index].clone();
-                                match runtime.call_function(f, &[value.clone()])? {
-                                    Bool(result) => {
-                                        if result {
-                                            Rc::make_mut(l).data_mut()[write_index] = value;
-                                            write_index += 1;
-                                        }
-                                    }
-                                    unexpected => {
-                                        return builtin_error!(
-                                            "list.filter expects a Bool to be returned from the \
-                                                    predicate, found '{}'",
-                                            value::type_as_string(&unexpected)
-                                        );
-                                    }
+                            f.args.len()
+                        );
+                    }
+                    let mut write_index = 0;
+                    for read_index in 0..list.data().len() {
+                        let value = list.data()[read_index].clone();
+                        match runtime.call_function(f, &[value.clone()])? {
+                            Bool(result) => {
+                                if result {
+                                    list.data_mut()[write_index] = value;
+                                    write_index += 1;
                                 }
                             }
-                            Rc::make_mut(l).data_mut().resize(write_index, Value::Empty);
+                            unexpected => {
+                                return builtin_error!(
+                                    "list.filter expects a Bool to be returned from the \
+                                                    predicate, found '{}'",
+                                    value::type_as_string(&unexpected)
+                                );
+                            }
                         }
-                        value => {
-                            Rc::make_mut(l).data_mut().retain(|x| x == value);
-                        }
-                    },
-                    unexpected => {
-                        return builtin_error!(
-                            "list.filter expects a reference to a\
-                                 list as its first argument, found {}",
-                            value::type_as_string(&unexpected)
-                        )
                     }
+                    list.data_mut().resize(write_index, Value::Empty);
                 }
+                value => {
+                    list.data_mut().retain(|x| x == value);
+                }
+            };
 
-                Ok(Value::Empty)
-            }
-            unexpected => builtin_error!(
-                "list.filter expects a reference to a list as its first argument, found {}",
-                value::type_as_string(unexpected)
-            ),
-        }
+            Ok(Value::Empty)
+        })
     });
 
     list.add_fn("transform", |runtime, args| {
-        if args.len() != 2 {
-            return builtin_error!("list.transform expects two arguments, found {}", args.len());
-        }
-
-        match &args[0] {
-            Ref(r) => {
-                match &mut *r.borrow_mut() {
-                    List(l) => match &args[1] {
-                        Function(f) => {
-                            if f.args.len() != 1 {
-                                return builtin_error!(
-                                    "The function passed to list.transform must have a \
+        list_op(args, 2, "transform", |list| match &args[1] {
+            Function(f) => {
+                if f.args.len() != 1 {
+                    return builtin_error!(
+                        "The function passed to list.transform must have a \
                                          single argument, found '{}'",
-                                    f.args.len()
-                                );
-                            }
+                        f.args.len()
+                    );
+                }
 
-                            for value in Rc::make_mut(l).data_mut().iter_mut() {
-                                *value = runtime.call_function(f, &[value.clone()])?;
-                            }
-                        }
-                        unexpected => {
-                            return builtin_error!(
-                                "list.transform expects a function as its \
-                                                   second argument, found '{}'",
-                                value::type_as_string(&unexpected)
-                            )
-                        }
-                    },
-                    unexpected => {
-                        return builtin_error!(
-                            "list.transform expects a reference to a\
-                                 list as its first argument, found {}",
-                            value::type_as_string(&unexpected)
-                        )
-                    }
+                for value in list.data_mut().iter_mut() {
+                    *value = runtime.call_function(f, &[value.clone()])?;
                 }
 
                 Ok(Value::Empty)
             }
             unexpected => builtin_error!(
-                "list.transform expects a reference to a list as its first argument, found {}",
-                value::type_as_string(unexpected)
+                "list.transform expects a function as its \
+                                                   second argument, found '{}'",
+                value::type_as_string(&unexpected)
             ),
-        }
+        })
     });
 
     list.add_fn("fold", |runtime, args| {
-        if args.len() != 3 {
-            return builtin_error!("list.fold expects three arguments, found {}", args.len());
-        }
-
-        match &args[0] {
-            Ref(r) => match &mut *r.borrow_mut() {
-                List(l) => match &args[2] {
-                    Function(f) => {
-                        if f.args.len() != 2 {
-                            return builtin_error!(
-                                "The function passed to list.fold must have two \
+        list_op(args, 3, "fold", |list| match &args[2] {
+            Function(f) => {
+                if f.args.len() != 2 {
+                    return builtin_error!(
+                        "The function passed to list.fold must have two \
                                      arguments, found '{}'",
-                                f.args.len()
-                            );
-                        }
+                        f.args.len()
+                    );
+                }
 
-                        let mut result = args[1].clone();
-                        for value in l.data().iter() {
-                            result = runtime.call_function(f, &[result, value.clone()])?;
-                        }
+                let mut result = args[1].clone();
+                for value in list.data().iter() {
+                    result = runtime.call_function(f, &[result, value.clone()])?;
+                }
 
-                        Ok(result)
-                    }
-                    unexpected => builtin_error!(
-                        "list.transform expects a function as its \
-                                                   second argument, found '{}'",
-                        value::type_as_string(&unexpected)
-                    ),
-                },
-                unexpected => builtin_error!(
-                    "list.fold expects a reference to a\
-                                 list as its first argument, found {}",
-                    value::type_as_string(&unexpected)
-                ),
-            },
+                Ok(result)
+            }
             unexpected => builtin_error!(
-                "list.fold expects a reference to a list as its first argument, found {}",
-                value::type_as_string(unexpected)
+                "list.transform expects a function as its \
+                                                   second argument, found '{}'",
+                value::type_as_string(&unexpected)
             ),
-        }
+        })
     });
 
     global.add_map("list", list);
+}
+
+fn list_op<'a>(
+    args: &[Value<'a>],
+    arg_count: usize,
+    op_name: &str,
+    mut op: impl FnMut(&mut ValueList<'a>) -> RuntimeResult<'a>,
+) -> RuntimeResult<'a> {
+    if args.len() < arg_count {
+        return builtin_error!(
+            "list.{} expects {} arguments, found {}",
+            op_name,
+            arg_count,
+            args.len()
+        );
+    }
+
+    match &args[0] {
+        Value::Ref(r) => match &mut *r.borrow_mut() {
+            Value::List(l) => op(Rc::make_mut(l)),
+            unexpected => builtin_error!(
+                "list.{} expects a reference to a list as its first argument, found {}",
+                op_name,
+                value::type_as_string(&unexpected)
+            ),
+        },
+        unexpected => builtin_error!(
+            "list.{} expects a reference to a list as its first argument, found {}",
+            op_name,
+            value::type_as_string(unexpected)
+        ),
+    }
 }
 
 fn list_is_sortable(list: &ValueList) -> bool {
