25beef093714bf4a4337b6d04892d0634ed96d2c
irh <ian.r.hobson@gmail.com>
2020-10-23 09:24:07 +0200

    Inline ValueList methods

--- src/runtime/src/value_list.rs
+++ src/runtime/src/value_list.rs
@@ -10,32 +10,39 @@ pub type ValueVec = smallvec::SmallVec<[Value; 4]>;
 pub struct ValueList(Arc<RwLock<ValueVec>>);
 
 impl ValueList {
+    #[inline]
     pub fn with_capacity(capacity: usize) -> Self {
         Self(Arc::new(RwLock::new(ValueVec::with_capacity(capacity))))
     }
 
+    #[inline]
     pub fn with_data(data: ValueVec) -> Self {
         Self(Arc::new(RwLock::new(data)))
     }
 
+    #[inline]
     pub fn from_slice(data: &[Value]) -> Self {
         Self(Arc::new(RwLock::new(
             data.iter().cloned().collect::<ValueVec>(),
         )))
     }
 
+    #[inline]
     pub fn len(&self) -> usize {
         self.data().len()
     }
 
+    #[inline]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    #[inline]
     pub fn data(&self) -> RwLockReadGuard<ValueVec> {
         self.0.read().unwrap()
     }
 
+    #[inline]
     pub fn data_mut(&self) -> RwLockWriteGuard<ValueVec> {
         self.0.write().unwrap()
     }
@@ -55,6 +62,7 @@ impl fmt::Display for ValueList {
 }
 
 impl PartialEq for ValueList {
+    #[inline]
     fn eq(&self, other: &Self) -> bool {
         *self.data() == *other.data()
     }
