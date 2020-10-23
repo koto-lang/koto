9bcee6c4da1c3a89aeb5884d681f8368c79e5f85
irh <ian.r.hobson@gmail.com>
2020-10-23 21:39:06 +0200

    Add range.start / end

--- src/runtime/src/core/range.rs
+++ src/runtime/src/core/range.rs
@@ -10,6 +10,11 @@ pub fn make_module() -> ValueMap {
         _ => external_error!("range.contains: Expected range and number as arguments"),
     });
 
+    result.add_fn("end", |vm, args| match vm.get_args(args) {
+        [Range(r)] => Ok(Number(r.end as f64)),
+        _ => external_error!("range.end: Expected range as argument"),
+    });
+
     result.add_fn("iter", |vm, args| match vm.get_args(args) {
         [Range(r)] => Ok(Iterator(ValueIterator::with_range(*r))),
         _ => external_error!("range.iter: Expected range as argument"),
@@ -18,6 +23,11 @@ pub fn make_module() -> ValueMap {
     result.add_fn("size", |vm, args| match vm.get_args(args) {
         [Range(r)] => Ok(Number((r.end - r.start) as f64)),
         _ => external_error!("range.size: Expected range as argument"),
+    });
+
+    result.add_fn("start", |vm, args| match vm.get_args(args) {
+        [Range(r)] => Ok(Number(r.start as f64)),
+        _ => external_error!("range.start: Expected range as argument"),
     });
 
     result
