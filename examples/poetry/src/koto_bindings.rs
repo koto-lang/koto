d4816dc82410a2191aff710708e87d7e885cc761
irh <ian.r.hobson@gmail.com>
2020-11-19 09:50:27 +0100

    Add Poetry example app

---
+++ examples/poetry/src/koto_bindings.rs
@@ -0,0 +1,109 @@
+use {
+    crate::Poetry,
+    koto::{
+        external_error, get_external_instance, is_external_instance, make_external_value,
+        type_as_string, visit_external_value, ExternalValue, Value, ValueIterator,
+        ValueIteratorOutput, ValueMap,
+    },
+    std::fmt,
+};
+
+pub fn make_module() -> ValueMap {
+    use Value::{Map, Str};
+
+    let mut result = ValueMap::new();
+
+    result.add_fn("new", {
+        |vm, args| match vm.get_args(args) {
+            [Str(text)] => {
+                let mut poetry = Poetry::default();
+                poetry.add_links(text);
+                Ok(Map(KotoPoetry::make_value_map(poetry)))
+            }
+            [unexpected] => external_error!(
+                "poetry.new: Expected a String as argument, found '{}'",
+                type_as_string(&unexpected),
+            ),
+            _ => external_error!("poetry.new: Expected a String as argument"),
+        }
+    });
+
+    result
+}
+
+#[derive(Debug)]
+pub struct KotoPoetry(Poetry);
+
+impl KotoPoetry {
+    fn make_value_map(poetry: Poetry) -> ValueMap {
+        use Value::*;
+
+        let mut result = ValueMap::default();
+
+        result.add_instance_fn("add_links", |vm, args| {
+            let args = vm.get_args(args);
+            get_external_instance!(args, "poetry", "next_word", Self, poetry, {
+                match args {
+                    [_, Str(text)] => {
+                        poetry.0.add_links(text);
+                        Ok(Empty)
+                    }
+                    _ => external_error!("poetry.add_links: Expected a String as argument"),
+                }
+            })
+        });
+
+        result.add_instance_fn("iter", |vm, args| match vm.get_args(args) {
+            [Map(poetry_map)] => {
+                if is_external_instance::<KotoPoetry>(poetry_map) {
+                    let poetry_map = poetry_map.clone();
+
+                    let iter = move || match visit_external_value(
+                        &poetry_map,
+                        |poetry: &mut KotoPoetry| {
+                            let result = poetry
+                                .0
+                                .next_word()
+                                .map_or(Empty, |word| Str(word.as_ref().into()));
+                            Ok(result)
+                        },
+                    ) {
+                        Ok(result) => Some(Ok(ValueIteratorOutput::Value(result))),
+                        Err(error) => Some(Err(error)),
+                    };
+
+                    Ok(Iterator(ValueIterator::make_external(iter)))
+                } else {
+                    external_error!("poetry.iter: Missing Poetry instance")
+                }
+            }
+            _ => external_error!("poetry.iter: Expected Poetry instance as argument"),
+        });
+
+        result.add_instance_fn("next_word", |vm, args| {
+            let args = vm.get_args(args);
+            get_external_instance!(args, "poetry", "next_word", Self, poetry, {
+                let result = match poetry.0.next_word() {
+                    Some(word) => Str(word.as_ref().into()),
+                    None => Empty,
+                };
+                Ok(result)
+            })
+        });
+
+        result.insert(Value::ExternalDataId, make_external_value(Self(poetry)));
+        result
+    }
+}
+
+impl ExternalValue for KotoPoetry {
+    fn value_type(&self) -> String {
+        "Poetry".to_string()
+    }
+}
+
+impl fmt::Display for KotoPoetry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Poetry")
+    }
+}
---
+++ examples/poetry/src/poetry.rs
@@ -0,0 +1,65 @@
+use {
+    indexmap::IndexMap,
+    rand::{seq::SliceRandom, thread_rng, Rng},
+    std::sync::Arc,
+};
+
+#[derive(Debug, Default)]
+pub struct Poetry {
+    links: IndexMap<Arc<str>, Vec<Arc<str>>>,
+    previous: Option<Arc<str>>,
+}
+
+impl Poetry {
+    pub fn add_links(&mut self, source: &str) {
+        let mut words =
+            source.split(|c: char| c.is_whitespace() || matches!(c, '(' | ')' | '[' | ']'));
+
+        if let Some(first) = words.next() {
+            let mut previous: Arc<str> = first.into();
+
+            for word in words {
+                if word.chars().any(char::is_alphabetic) {
+                    let word: Arc<str> = word.into();
+                    self.links
+                        .entry(previous.clone())
+                        .or_insert_with(|| Vec::new())
+                        .push(word.clone());
+                    previous = word;
+                }
+            }
+        }
+    }
+
+    pub fn next_word(&mut self) -> Option<Arc<str>> {
+        let result = self
+            .previous
+            .as_ref()
+            .map(|previous| {
+                // Given a previous word, find its links
+                self.links
+                    .get(previous)
+                    .map(|words| {
+                        // Given some links, choose the next word
+                        let mut rng = thread_rng();
+                        words.choose(&mut rng)
+                    })
+                    .unwrap_or(None)
+            })
+            .unwrap_or(None);
+
+        let result = if let Some(result) = result {
+            Some(result.clone())
+        } else {
+            // If no link was found, choose a new starting point
+            let start = thread_rng().gen_range(0, self.links.len());
+            self.links
+                .get_index(start)
+                .map(|(key, _value)| key)
+                .cloned()
+        };
+
+        self.previous = result.clone();
+        result
+    }
+}
