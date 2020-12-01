73617b43491efdcae945fc83a952958d2732c1cf
irh <ian.r.hobson@gmail.com>
2020-12-01 15:03:53 +0100

    Add a simple example test

---
+++ src/koto/tests/one_plus_two.rs
@@ -0,0 +1,22 @@
+#![allow(clippy::float_cmp)]
+
+use koto::{Koto, Value};
+
+#[test]
+fn one_plus_two() {
+    let mut koto = Koto::default();
+    match koto.compile("1 + 2") {
+        Ok(_) => match koto.run() {
+            Ok(result) => match result {
+                Value::Number(n) => assert_eq!(n, 3.0),
+                other => panic!("Unexpected result: {}", other),
+            },
+            Err(runtime_error) => {
+                panic!("Runtime error: {}", runtime_error);
+            }
+        },
+        Err(compiler_error) => {
+            panic!("Compiler error: {}", compiler_error);
+        }
+    }
+}
