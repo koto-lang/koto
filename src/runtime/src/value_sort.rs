5b64f7a4cbdd7d32f36ff8cfb0267621870a33d6
Ales Tsurko <ales.tsurko@gmail.com>
2021-03-03 22:39:55 +0300

    Fix list.sort by key

--- src/runtime/src/value_sort.rs
+++ src/runtime/src/value_sort.rs
@@ -1,8 +1,12 @@
+//! As operators can be overloaded, we can't treat values specifically (i.e. use `PartialOrd` and
+//! `Ord` for example). So we always need to call operators to compare them. This module contains
+//! helpers for comparing and sorting [Value].
+
 use std::cmp::Ordering;
 
 use crate::{Operator, RuntimeError, Value, Vm};
 
-/// Used internally in sorting methods implementations.
+/// An implementation of the quick sort algorithm for a slice of [Value].
 pub fn quick_sort(
     vm: &mut Vm,
     arr: &mut [Value],
@@ -36,7 +40,7 @@ fn partition(
     let mut i = start;
 
     while i < end {
-        if is_less(vm, arr[i].clone(), pivot.clone())? {
+        if let Ordering::Less = cmp(vm, &arr[i], &pivot)? {
             arr.swap(i, index);
             index += 1;
         }
@@ -49,14 +53,60 @@ fn partition(
     Ok(index)
 }
 
-fn is_less(vm: &mut Vm, a: Value, b: Value) -> Result<bool, RuntimeError> {
-    match vm.run_binary_op(Operator::Less, a, b)? {
-        Value::Bool(val) => Ok(val),
-        _ => unreachable!(),
+/// The same as [quick_sort], but sorts the `arr` by provided `key`.
+///
+/// Notice, this function doesn't check equality of sizes between arrays.
+pub fn quick_sort_by_key(
+    vm: &mut Vm,
+    key: &mut [Value],
+    arr: &mut [Value],
+    start: usize,
+    end: usize,
+) -> Result<(), RuntimeError> {
+    if start >= end {
+        return Ok(());
     }
+
+    let pivot = partition_with_key(vm, key, arr, start, end)?;
+
+    if pivot < 1 {
+        return Ok(());
+    }
+
+    quick_sort_by_key(vm, key, arr, start, pivot - 1)?;
+    quick_sort_by_key(vm, key, arr, pivot + 1, end)?;
+
+    Ok(())
 }
 
-/// Replace for `Ord::cmp`.
+fn partition_with_key(
+    vm: &mut Vm,
+    key: &mut [Value],
+    arr: &mut [Value],
+    start: usize,
+    end: usize,
+) -> Result<usize, RuntimeError> {
+    let pivot = key[end].clone();
+    let mut index = start;
+    let mut i = start;
+
+    while i < end {
+        if let Ordering::Less = cmp(vm, &key[i], &pivot)? {
+            key.swap(i, index);
+            arr.swap(i, index);
+            index += 1;
+        }
+
+        i += 1;
+    }
+
+    key.swap(index, end);
+    arr.swap(index, end);
+
+    Ok(index)
+}
+
+/// Replaces `Ord::cmp` in implementations where `Ordering` needed.
 pub fn cmp(vm: &mut Vm, a: &Value, b: &Value) -> Result<Ordering, RuntimeError> {
     if let Value::Bool(true) = vm.run_binary_op(Operator::Equal, a.clone(), b.clone())? {
         return Ok(Ordering::Equal);
