6c8aed2eec63b2c2e8254c4e4616a8badbba1e7a
Ales Tsurko <ales.tsurko@gmail.com>
2021-03-12 14:52:37 +0300

    Bring back koto.script_dir and koto.script_path

--- src/runtime/src/core/koto.rs
+++ src/runtime/src/core/koto.rs
@@ -19,6 +19,9 @@ pub fn make_module() -> ValueMap {
         Ok(Value::Map(vm.context_mut().global.clone()))
     });
 
+    result.add_value("script_dir", Str("".into()));
+    result.add_value("script_path", Str("".into()));
+
     result.add_fn("type", |vm, args| match vm.get_args(args) {
         [value] => Ok(Str(type_as_string(value).into())),
         _ => external_error!("koto.type: Expected single argument"),
