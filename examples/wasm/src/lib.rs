fee81e12f4316127e967cec21892a29bb4e07d0f
Ian Hobson <ian.r.hobson@gmail.com>
2022-11-04 14:18:02 +0100

    Don't use wee_alloc in the wasm example

--- examples/wasm/src/lib.rs
+++ examples/wasm/src/lib.rs
@@ -7,9 +7,6 @@ use {
     wasm_bindgen::prelude::*,
 };
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 // Captures output from Koto in a String
 #[derive(Debug)]
 struct OutputCapture {
