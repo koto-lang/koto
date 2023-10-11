4112552a85e8fa4581ceae0c10c608be0c398e44
Ian Hobson <ian.r.hobson@gmail.com>
2023-10-11 16:18:43 +0200

    Use anyhow in the poetry example

--- examples/poetry/src/main.rs
+++ examples/poetry/src/main.rs
@@ -1,6 +1,7 @@
 mod koto_bindings;
 mod poetry;
 
+use anyhow::{bail, Context, Error, Result};
 use hotwatch::{
     blocking::{Flow, Hotwatch},
     Event,
@@ -8,26 +9,11 @@ use hotwatch::{
 use koto::{Koto, KotoSettings};
 use poetry::*;
 use std::{
-    error::Error,
-    fmt, fs,
+    fs,
     path::{Path, PathBuf},
     str::FromStr,
     time::Duration,
 };
-
-#[derive(Debug)]
-struct PoetryError {
-    prefix: String,
-    error: Box<dyn Error>,
-}
-
-impl fmt::Display for PoetryError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}: {}", self.prefix, self.error)
-    }
-}
-
-impl Error for PoetryError {}
 
 fn version_string() -> String {
     format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
@@ -60,7 +46,7 @@ struct PoetryArgs {
     watch: bool,
 }
 
-fn parse_arguments() -> Result<PoetryArgs, String> {
+fn parse_arguments() -> Result<PoetryArgs> {
     let mut args = pico_args::Arguments::from_env();
 
     let help = args.contains(["-h", "--help"]);
@@ -68,7 +54,7 @@ fn parse_arguments() -> Result<PoetryArgs, String> {
     let watch = args.contains(["-w", "--watch"]);
     let script = args
         .value_from_str(["-s", "--script"])
-        .map_err(|_| "Missing script argument".to_string())?;
+        .context("Missing script argument")?;
 
     Ok(PoetryArgs {
         help,
@@ -78,7 +64,7 @@ fn parse_arguments() -> Result<PoetryArgs, String> {
     })
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let args = match parse_arguments() {
         Ok(args) => {
             if args.help {
@@ -93,7 +79,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         Err(error) => {
             println!("{}\n\n{}", help_string(), error);
-            return Err("Failed to parse arguments".to_string().into());
+            bail!("Failed to parse arguments");
         }
     };
 
@@ -106,7 +92,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .add_map("poetry", koto_bindings::make_module());
     koto.prelude().add_map("random", koto_random::make_module());
 
-    let script_path = PathBuf::from_str(&args.script).expect("Failed to parse script path");
+    let script_path = PathBuf::from_str(&args.script).context("Failed to parse script path")?;
     koto.set_script_path(Some(script_path.clone()))
         .expect("Failed to set script path");
 
@@ -116,7 +102,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         let mut hotwatch = Hotwatch::new_with_custom_delay(Duration::from_secs_f64(0.25))
-            .expect("Failed to initialize file watcher");
+            .context("Failed to initialize file watcher")?;
         hotwatch
             .watch(&args.script, move |event: Event| {
                 match event {
@@ -129,35 +115,26 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
                 Flow::Continue
             })
-            .expect("failed to watch file!");
+            .context("Failed to watch file!")?;
         hotwatch.run();
         Ok(())
     } else {
-        match compile_and_run(&mut koto, &script_path) {
-            Ok(_) => Ok(()),
-            Err(error) => {
-                eprintln!("{error}");
-                std::process::exit(1);
-            }
-        }
+        compile_and_run(&mut koto, &script_path)
     }
 }
 
-fn compile_and_run(koto: &mut Koto, script_path: &Path) -> Result<(), Box<dyn Error>> {
+fn compile_and_run(koto: &mut Koto, script_path: &Path) -> Result<()> {
     let script = fs::read_to_string(script_path)?;
-    match koto.compile(&script) {
-        Ok(_) => match koto.run() {
-            Ok(_) => Ok(()),
-            Err(e) => Err(PoetryError {
-                prefix: "Error while running script".into(),
-                error: e.into(),
-            }
-            .into()),
-        },
-        Err(e) => Err(PoetryError {
-            prefix: "Error while compiling script".into(),
-            error: e.into(),
-        }
-        .into()),
-    }
+    koto.compile(&script)
+        .map_err(koto_error_to_anyhow)
+        .context("Error while compiling script")?;
+    koto.run()
+        .map_err(koto_error_to_anyhow)
+        .context("Error while running script")?;
+    Ok(())
+}
+
+fn koto_error_to_anyhow(e: koto::Error) -> anyhow::Error {
+    // koto::Error doesn't implement Send, which anyhow requires, so render the error to a String
+    Error::msg(e.to_string())
 }
