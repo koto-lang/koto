koto-lang/koto

commit 45d3328661dba46dc8fea51cd53ef5705422a0f7

Allow export to be used with any iterable that yields key/value pairs

crates/runtime/src/vm.rs
@@ -746,7 +746,8 @@ impl KotoVm {
                 self.set_register(register, string.into());
             }
             LoadNonLocal { register, constant } => self.run_load_non_local(register, constant)?,
-            ValueExport { name, value } => self.run_value_export(name, value)?,
+            ExportValue { key, value } => self.run_export_value(key, value)?,
+            ExportEntry { entry } => self.run_export_entry(entry)?,
             Import { register } => self.run_import(register)?,
             MakeTempTuple {
                 register,
@@ -1003,10 +1004,34 @@ impl KotoVm {
         }
     }
 
-    fn run_value_export(&mut self, name_register: u8, value_register: u8) -> Result<()> {
-        let name = ValueKey::try_from(self.clone_register(name_register))?;
+    fn run_export_value(&mut self, key_register: u8, value_register: u8) -> Result<()> {
+        let key = ValueKey::try_from(self.clone_register(key_register))?;
         let value = self.clone_register(value_register);
-        self.exports.data_mut().insert(name, value);
+        self.exports.data_mut().insert(key, value);
+        Ok(())
+    }
+
+    fn run_export_entry(&mut self, entry_register: u8) -> Result<()> {
+        let maybe_entry = self.clone_register(entry_register);
+        let maybe_key_value_pair = match &maybe_entry {
+            KValue::Tuple(tuple) => match tuple.data() {
+                [key, value] => Some((key.clone(), value.clone())),
+                _ => None,
+            },
+            KValue::TemporaryTuple(temp_tuple) => {
+                match self.register_slice(temp_tuple.start, temp_tuple.count) {
+                    [key, value] => Some((key.clone(), value.clone())),
+                    _ => None,
+                }
+            }
+            _ => None,
+        };
+        let Some((key, value)) = maybe_key_value_pair else {
+            return unexpected_type("Key/Value pair to export", &maybe_entry);
+        };
+        self.exports
+            .data_mut()
+            .insert(ValueKey::try_from(key)?, value);
         Ok(())
     }
 
@@ -1179,12 +1204,13 @@ impl KotoVm {
                     match iterator.next() {
                         Some(KIteratorOutput::Value(value)) => Some(value),
                         Some(KIteratorOutput::ValuePair(first, second)) => {
-                            if let Some(result) = result_register {
+                            if result_register.is_some() {
                                 if output_is_temporary {
-                                    self.set_register(result + 1, first);
-                                    self.set_register(result + 2, second);
+                                    let start_register = self.next_register();
+                                    self.registers.push(first);
+                                    self.registers.push(second);
                                     Some(TemporaryTuple(RegisterSlice {
-                                        start: result + 1,
+                                        start: start_register,
                                         count: 2,
                                     }))
                                 } else {
@@ -3419,6 +3445,7 @@ impl KotoVm {
         (self.registers.len() - self.register_base) as u8
     }
 
+    // Sets the register, which must already be available in the stack
     fn set_register(&mut self, register: u8, value: KValue) {
         let index = self.register_index(register);
         self.registers[index] = value;

crates/bytecode/src/compiler.rs
@@ -158,6 +158,18 @@ impl<'a> CompileNodeContext<'a> {
         }
     }
 
+    fn with_fixed_register_or_any(self) -> Self {
+        let result_register = if matches!(self.result_register, ResultRegister::Fixed(_)) {
+            self.result_register
+        } else {
+            ResultRegister::Any
+        };
+        Self {
+            result_register,
+            ..self
+        }
+    }
+
     fn compile_for_side_effects(self) -> Self {
         Self {
             result_register: ResultRegister::None,
@@ -1344,7 +1356,7 @@ impl Compiler {
     fn compile_value_export(&mut self, id: ConstantIndex, value_register: u8) -> Result<()> {
         let id_register = self.push_register()?;
         self.compile_load_string_constant(id_register, id);
-        self.push_op(Op::ValueExport, &[id_register, value_register]);
+        self.push_op(Op::ExportValue, &[id_register, value_register]);
         self.pop_register()?;
         self.frame_mut().add_to_exported_ids(id);
         Ok(())
@@ -1580,11 +1592,34 @@ impl Compiler {
                 targets,
                 expression,
             } => self.compile_multi_assign(targets, *expression, true, ctx),
+            // Maps can be exported directly rather than relying on the iterator logic below
             Node::Map(entries) => self.compile_make_map(entries, true, ctx),
-            unexpected => self.error(ErrorKind::UnexpectedNode {
-                expected: "an assignment or a Map to export".into(),
-                unexpected: unexpected.clone(),
-            }),
+            // Other expressions can be evaluated and then assumed to be iterable
+            _ => {
+                // Evaluate the expression and convert the result into an iterator
+                let result = self.compile_node(expression, ctx.with_fixed_register_or_any())?;
+                let expression_register = result.unwrap(self)?;
+                let stack_count = self.stack_count();
+                let iterator_register = self.push_register()?;
+                self.push_op(Op::MakeIterator, &[iterator_register, expression_register]);
+
+                // Consume the expression iterator, exporting each output entry
+                let iter_start_ip = self.bytes.len();
+                let output_register = self.push_register()?;
+                self.push_op(Op::IterNextTemp, &[output_register, iterator_register]);
+                let iter_finished_offset = self.push_offset_placeholder();
+
+                self.push_op(Op::ExportEntry, &[output_register]);
+
+                // Jump back to get more iterator output
+                self.push_jump_back_op(Op::JumpBack, &[], iter_start_ip);
+
+                // Finished, update the IterNextTemp offset and clean up the temporary registers
+                self.update_offset_placeholder(iter_finished_offset)?;
+                self.truncate_register_stack(stack_count)?;
+
+                Ok(result)
+            }
         }
     }
 
@@ -2928,7 +2963,7 @@ impl Compiler {
                 }
 
                 if export_entry {
-                    self.push_op_without_span(ValueExport, &[key_register, value_register]);
+                    self.push_op_without_span(ExportValue, &[key_register, value_register]);
                 }
 
                 self.pop_register()?;
@@ -2945,7 +2980,7 @@ impl Compiler {
                 }
 
                 if export_entry {
-                    self.push_op_without_span(ValueExport, &[key_register, value_register]);
+                    self.push_op_without_span(ExportValue, &[key_register, value_register]);
                 }
 
                 self.pop_register()?;

crates/bytecode/src/instruction.rs
@@ -45,9 +45,12 @@ pub enum Instruction {
         register: u8,
         constant: ConstantIndex,
     },
-    ValueExport {
-        name: u8,
+    ExportValue {
+        key: u8,
         value: u8,
+    },
+    ExportEntry {
+        entry: u8,
     },
     Import {
         register: u8,
@@ -561,9 +564,10 @@ impl fmt::Debug for Instruction {
                     "LoadNonLocal    result: {register:<7} constant: {constant}"
                 )
             }
-            ValueExport { name, value } => {
-                write!(f, "ValueExport     name: {name:<7} value: {value}")
+            ExportValue { key, value } => {
+                write!(f, "ExportValue     key: {key:<10} value: {value}")
             }
+            ExportEntry { entry } => write!(f, "ExportEntry     entry: {entry}"),
             Import { register } => write!(f, "Import          register: {register}"),
             MakeTempTuple {
                 register,

crates/bytecode/src/instruction_reader.rs
@@ -183,10 +183,11 @@ impl Iterator for InstructionReader {
                 register: byte_a,
                 constant: get_var_u32!().into(),
             },
-            Op::ValueExport => ValueExport {
-                name: byte_a,
+            Op::ExportValue => ExportValue {
+                key: byte_a,
                 value: get_u8!(),
             },
+            Op::ExportEntry => ExportEntry { entry: byte_a },
             Op::Import => Import { register: byte_a },
             Op::MakeTempTuple => {
                 let [byte_b, byte_c] = get_u8x2!();

crates/bytecode/src/op.rs
@@ -470,12 +470,21 @@ pub enum Op {
     /// `[*key, *name, *value]`
     MetaExportNamed,
 
-    /// Exports a value by adding it to the module's exports map
+    /// Exports a key/value pair by adding it to the module's exports map
     ///
     /// Used for expressions like `export foo = ...`
     ///
-    /// `[*name, *value]`
-    ValueExport,
+    /// `[*key, *value]`
+    ExportValue,
+
+    /// Exports an entry by adding it to the module's exports map
+    ///
+    /// - If the entry is a tuple, then it's assumed to be a key/value pair.
+    /// - If the entry is an iterator, then it's unpacked into a key/value pair.
+    /// - Otherwise an error will be thrown.
+    ///
+    /// `[*entry]`
+    ExportEntry,
 
     /// Accesses a contained value via a constant key
     ///
@@ -566,7 +575,6 @@ pub enum Op {
     CheckOptionalType,
 
     // Unused opcodes, allowing for a direct transmutation from a byte to an Op.
-    Unused89,
     Unused90,
     Unused91,
     Unused92,

crates/bytecode/tests/compile_failures.rs
@@ -137,26 +137,6 @@ match [1, 2, 3]
             }
         }
 
-        mod export {
-            use super::*;
-
-            #[test]
-            fn id_without_assignment() {
-                let source = "
-export x
-";
-                check_compilation_fails(source);
-            }
-
-            #[test]
-            fn list() {
-                let source = "
-export [1, 2, 3]
-";
-                check_compilation_fails(source);
-            }
-        }
-
         mod functions {
             use super::*;
 

crates/runtime/src/types/tuple.rs
@@ -40,6 +40,11 @@ impl KTuple {
         }
     }
 
+    /// Returns the tuple's values as a slice
+    pub fn data(&self) -> &[KValue] {
+        self.deref()
+    }
+
     /// Returns true if the tuple contains only immutable values
     pub fn is_hashable(&self) -> bool {
         self.iter().all(KValue::is_hashable)

crates/runtime/src/types/value.rs
@@ -180,7 +180,7 @@ impl KValue {
                 |o| o.type_string(),
             ),
             Iterator(_) => "Iterator".into(),
-            TemporaryTuple { .. } => "Temporary_tuple".into(),
+            TemporaryTuple { .. } => "TemporaryTuple".into(),
         }
     }
 

commit 1aad01b0a58fe8eb2bba4dd0668f3daa3608fbf4

Fix overflows when using iterators that output pairs in a for loop
This fixes a regression introduced in
45d3328661dba46dc8fea51cd53ef5705422a0f7 that caused register overflows
when using a for loop with iterators that output value pairs.

The idea in 45d33 was to push temporary registers onto the stack to
avoid assuming that it was safe to assign directly in to the stack.
This doesn't work with temporary tuples however because there's currently
no mechanism to clean up the pushed registers later on, which caused
the stack to grow rapidly.

Rather than introduce a new cleanup mechanism, here we roll back to the
previous approach but ensure that the the stack has available slots for
the temporary tuple contents. This might not be sufficient in the long
run but at the moment I can't think of a case where this won't hold up,
so it'll do for now.

crates/runtime/src/vm.rs
@@ -1212,15 +1212,22 @@ impl KotoVm {
                     match iterator.next() {
                         Some(KIteratorOutput::Value(value)) => Some(value),
                         Some(KIteratorOutput::ValuePair(first, second)) => {
-                            if result_register.is_some() {
+                            if let Some(result) = result_register {
                                 if output_is_temporary {
-                                    let start_register = self.next_register();
-                                    self.registers.push(first);
-                                    self.registers.push(second);
-                                    Some(TemporaryTuple(RegisterSlice {
-                                        start: start_register,
-                                        count: 2,
-                                    }))
+                                    // Place the value pair in a temporary tuple following the
+                                    // result register. The assumption here is that the values
+                                    // following the result register are available for re-use,
+                                    // if that turns out to not be true in all cases then a
+                                    // different approach will be needed.
+                                    let start = result + 1;
+                                    let first_index = self.register_index(start);
+                                    let second_index = first_index + 1;
+                                    if second_index >= self.registers.len() {
+                                        self.registers.resize(second_index + 1, KValue::Null);
+                                    }
+                                    self.registers[first_index] = first;
+                                    self.registers[second_index] = second;
+                                    Some(TemporaryTuple(RegisterSlice { start, count: 2 }))
                                 } else {
                                     Some(Tuple(vec![first, second].into()))
                                 }

crates/runtime/tests/iterator_tests.rs
@@ -457,6 +457,19 @@ y.next().get()
 ";
             check_script_output(script, number_tuple(&[3, 13]));
         }
+
+        #[test]
+        fn for_loop_over_many_zipped_values() {
+            // This ensures that unpacking temporary value pairs in a for loop
+            // doesn't overflow the register stack.
+            let script = "
+r = 1..=128
+for a, b in r.zip r
+  () # no-op
+b
+";
+            check_script_output(script, 128);
+        }
     }
 }
 