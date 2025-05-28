f32a5e1a8e3c9c38004004444f67909332e0159a
Ian Hobson <ian.r.hobson@gmail.com>
2025-05-28 09:41:29 +0200

    Don't force break the first access in a broken chain expression

--- crates/format/tests/format_tests.rs
+++ crates/format/tests/format_tests.rs
@@ -972,8 +972,7 @@ foo
 foo.bar()?.'baz'().xyz[0]?.abc()
 "],
                 "\
-foo
-  .bar()?
+foo.bar()?
   .'baz'()
   .xyz[0]?
   .abc()
@@ -985,7 +984,8 @@ foo
         fn broken_by_line_length() {
             check_format_output_with_options(
                 &["\
-foo.bar[  #- foo -# ..9  ]?.baz( 1 ,  2 ,  3..=4  )
+foo.bar[  #- foo -# ..9  ]?
+  .baz( 1 ,  2 ,  3..=4  )
 "],
                 "\
 foo
@@ -1001,7 +1001,7 @@ foo
 
         #[test]
         fn paren_free_call_before_end() {
-            // The paren-free call prevents collaps
+            // The paren-free call prevents collapse
             check_format_output(
                 &["\
 foo
